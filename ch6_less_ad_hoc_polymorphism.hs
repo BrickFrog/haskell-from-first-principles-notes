8bf9a4c08ee370cf29460ec50b6831a522676cfc
Justin <brickfrog@users.noreply.github.com>
2021-01-14 23:55:10 -0500

    1/14 Reading

--- ch6_less_ad_hoc_polymorphism.hs
+++ ch6_less_ad_hoc_polymorphism.hs
@@ -34,10 +34,42 @@ data Person = Person Bool deriving (Show)
 printPerson :: Person -> IO ()
 printPerson person = putStrLn (show person)
 
--- 2)
+-- 2) Needed to derive Eq for equality check
+-- 3) Blah/Woot. You need Ord to use > and 9 has no num instance
 data Mood = Blah
-            | Woot deriving (Show, Eq)
+            | Woot deriving (Show, Eq, Ord)
 
 settleDown x = if x == Woot
                 then Blah
                 else x
+
+-- 4) missing arguments for s1
+type Subject = String 
+type Verb = String
+type Object = String
+
+data Sentence =
+    Sentence Subject Verb Object
+    deriving (Eq, Show)
+
+s1 = Sentence "dogs" "drool"
+s2 = Sentence "Julie" "loves" "dogs"
+
+-- Matching the Types
+
+--1) Num byitself needs typecheck, would work with Integer 
+i :: Num a => a
+i = 1
+
+
+--2+3) replace with Num a => a doesn't work, needs fractional
+f :: Float 
+f = 1.0
+
+f' :: Fractional a => a
+f' = 1.0
+
+
+-- Type - Kwon do Sampling
+chk :: Eq b => (a -> b) -> a -> b -> Bool
+chk f x y = f x == y
