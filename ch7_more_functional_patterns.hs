4ac0f99f1a2c0fe1cc042ddd382e26108483d7e2
Justin <brickfrog@users.noreply.github.com>
2021-01-15 23:53:40 -0500

    1/15

--- ch7_more_functional_patterns.hs
+++ ch7_more_functional_patterns.hs
@@ -1,1 +1,10 @@
 -- Chapter 7 - More Functional Patterns
+
+myNum :: Num a => a
+myNum = 1
+
+myVal :: Num a => a -> a
+myVal f = f + myNum
+
+stillAFunction :: [a] -> [a] -> [a] -> [a]
+stillAFunction a b c = a ++ b ++ c
